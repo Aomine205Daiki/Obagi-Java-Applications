4c22b38f1ce82cf07b26f29acd962a9b4a3b8e7c
Aomine205Daiki <patrickknowledge4@gmail.com>
2025-03-20 15:40:18 -0600

    UserInput

---
+++ TypeCasting.java
@@ -0,0 +1,9 @@
+public class TypeCasting{
+	public static void main(String[] args){
+		//Widening Type Casting
+		double balance = 58585;
+		System.out.printf("Your account balance is %c%f%n",'S',balance);
+		
+		//Narrowing Type Casting
+	}
+}
