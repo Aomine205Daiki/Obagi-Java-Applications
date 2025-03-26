c2811b0a75112ea3fc9aadbbe8ed2c03809bd203
Aomine205Daiki <patrickknowledge4@gmail.com>
2025-03-26 13:48:15 -0600

    Operators

--- TypeCasting.java
+++ TypeCasting.java
@@ -4,6 +4,13 @@ public class TypeCasting{
 		double balance = 58585;
 		System.out.printf("Your account balance is %c%f%n",'S',balance);
 		
-		//Narrowing Type Casting
+		//Narrowing Type Casting(ASCII code ? = 63)
+		char alpha = '?';
+		
+		int asciiCode = (char)alpha;
+		System.out.printf("The ASCII code for ? is %d%n",asciiCode);
+		
+		double height = 5.8;
+		short
 	}
 }
